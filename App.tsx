a0233b1665bef22b9531f71c90b417102ff9a523
Yaman KATBY <m.yaman.katby@gmail.com>
2019-10-02 15:40:43 +0300

    🏷 Migrate to TypeScript

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+interface Props {
+
+}
+
+const App = (props: Props) => {
+	return (
+		<View>
+			<Text>Hi, App Component!</Text>
+		</View>
+	);
+};
+
+export default App;
